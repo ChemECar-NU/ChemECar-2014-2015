4107eb78020debda4cb1a95c185f07790d98b9bc
heyyeh12 <heyyeh12@aol.com>
2014-11-16 12:20:32 -0600

    photoresistor testing code

---
+++ photoresistorTest/photoresistorTest.ino
@@ -0,0 +1,39 @@
+/*
+Photoresistor
+*/
+
+// Photoresistor analog pin
+const int sensorPin = 0;
+// LED digital pin
+const int ledPin = 9;
+// threshold sensor value for on/off --- needs to be determined
+const int threshold = 400;
+// autoTune init values
+int lightLevel;
+
+void setup()
+{
+  // create serial connection to use Serial Monitor
+  Serial.begin(9600);
+  // setup LED pin
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  // read value from photoresistor
+  lightLevel = analogRead(sensorPin);
+  
+  // prints value in Serial Monitor
+  Serial.print("Photoresistor: ");
+  Serial.println(lightLevel);
+  
+  if (lightLevel < threshold) {
+    // if not dark, LED turns on
+    analogWrite(ledPin, 255);
+  } else {
+    // if dark, LED turns off
+    analogWrite(ledPin, 0);
+  }
+}
+
